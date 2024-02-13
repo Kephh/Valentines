9cf6d27daa99aea35493b0ef2d2f592f014f028b
Kaif Khan <92052609+Kephh@users.noreply.github.com>
2024-02-14 01:58:30 +0530

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,103 @@
+var video = document.querySelector("video");
+var flag = true;
+
+var random = Math.floor(Math.random() * 2);
+document.querySelector("#btn").addEventListener("click", function () {
+    if (document.querySelector("input").value != "") {
+        if (random == 0) {
+            document.querySelector(".overlay").style.display = "none";
+            setTimeout(function () {
+                document.querySelector(".msg").style.display = "flex";
+            }, 1000);
+
+            setTimeout(function () {
+                document.querySelector(".msg").style.display = "none";
+                video.style.display = "block";
+                video.play();
+            }, 2500);
+
+            setTimeout(function () {
+                document.querySelector(".p-cls").style.display = "flex";
+                document.querySelector(".btn-cls").style.display = "flex";
+            }, 4000);
+
+            document.getElementById("yes-btn").addEventListener("click", function () {
+                document.querySelector("#q-p").style.display = "none";
+                document.querySelector(".p-cls").style.backgroundColor = "transparent";
+                document.querySelector(".btn-cls").style.display = "none";
+
+                setTimeout(function () {
+                    document.querySelector("#alrighty").style.display = "block";
+                    document.querySelector(".p-cls").style.backgroundColor = "white";
+                }, 1000);
+
+                setTimeout(function () {
+                    video.currentTime = 42.8;
+                    document.querySelector("#alrighty").style.display = "none";
+                    document.querySelector("#dynamic-text2").style.display = "flex";
+                }, 2000);
+            })
+
+            document.getElementById("no-btn").addEventListener("click", function () {
+                if (flag) {
+                    flag = false;
+                    document.querySelector("#q-p").style.display = "none";
+                    document.querySelector(".p-cls").style.backgroundColor = "transparent";
+                    document.querySelector(".btn-cls").style.display = "none";
+                    setTimeout(function () {
+                        document.querySelector("#q-p").style.display = "flex";
+                        document.querySelector(".p-cls").style.backgroundColor = "white";
+                        document.querySelector(".btn-cls").style.display = "flex";
+                        document.querySelector("#q-p").innerHTML = "You don't like me back...?";
+                        yes.innerText = "I do";
+                        no.innerText = "I don't";
+                    }, 1000);
+                } else {
+                    document.querySelector("#q-p").style.display = "none";
+                    document.querySelector(".p-cls").style.backgroundColor = "transparent";
+                    document.querySelector(".btn-cls").style.display = "none";
+                    setTimeout(function () {
+                        document.querySelector("#q-p").style.display = "flex";
+                        document.querySelector(".p-cls").style.backgroundColor = "white";
+                        document.querySelector("#q-p").innerHTML = "At least one of us is honest!";
+                        setTimeout(function () {
+                            closeWindowOrRedirect();
+                        }, 2500);
+                    }, 1000);
+                }
+
+            })
+        } else {
+            setTimeout(function () {
+                document.querySelector(".msg").style.display = "block";
+                document.querySelector(".msg").style.backgroundColor = "transparent";
+                document.querySelector("#msg-p").innerText = "I don't like you";
+                setTimeout(function () {
+                    document.querySelector(".msg").style.display = "none";
+                }, 1500);
+                setTimeout(function () {
+                    document.querySelector(".msg").style.display = "block";
+                    document.querySelector(".msg").style.backgroundColor = "transparent";
+                    document.querySelector("#msg-p").innerText = "Just Kidding!!";
+                }, 2500);
+                setTimeout(function () {
+                    document.querySelector("#btn").innerText = "Try Again";
+                }, 3500);
+            }, 1000);
+            document.querySelector("#btn").addEventListener("click", function () {
+                window.location.reload();
+            })
+        }
+    } else {
+        alert("Please enter your name");
+    }
+})
+
+function closeWindowOrRedirect() {
+    var userAgent = navigator.userAgent.toLowerCase();
+    if (/iphone|ipad|ipod|android|blackberry|mini|windows\sce|palm/i.test(userAgent)) {
+        window.location.href = 'about:blank';
+    } else {
+        window.close();
+    }
+}
